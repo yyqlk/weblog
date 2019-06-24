047f8a7c7c728252ac0e73cb4be8bf5db15c48f6
like <like@qq.com>
2019-06-24 14:59:55 +0800

    init rep

---
+++ src/test/java/com/like/weblog/weblog/WeblogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.like.weblog.weblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeblogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
